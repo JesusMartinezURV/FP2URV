6f14ab30e55fab84181f54ca7dee039b4c6eff5f
JesusMartinezURV <jesuscristhofer.martinez@estudiants.urv.cat>
2024-04-22 09:23:03 +0200

    Creacion de funciones y ficheros basicos

---
+++ PracticaFP/Funciones/funciones.h
@@ -0,0 +1,32 @@
+
+#DEFINE MIN_USERS 0
+#DEFINE MAX_USERS 10000
+
+struct user
+{
+    data_0_numero
+    data1_nom
+    data2_poblacio
+    data3_sexe
+    data4_data_naix
+}
+
+
+// demanar el numero d'usuari
+// check del rang (0,MAX]
+// buscar num usuari al arxiu usuaris text
+// guardar les dades corresponents a stuct user
+
+void guardar_data_usuari(int num)
+{
+
+}
+
+// se le pasa un struct con los datos guardados en el struct user
+// de la funcion guardar_data_user
+void mostrar usuari ()
+{
+
+}
+
+
