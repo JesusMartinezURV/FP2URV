0fafcbd5ae4fc9f1e1c6b0e925a7476c47257126
JesusMartinezURV <jesuscristhofer.martinez@estudiants.urv.cat>
2024-04-22 09:31:50 +0200

    Set up del main

--- PracticaFP/main.c
+++ PracticaFP/main.c
@@ -1,1 +1,15 @@
-vfv
+#include "math.h"
+#include "stdlib.h"
+
+int main(int argc, char *argv[])
+{}
+    if (argc != 2)
+    {
+        printf("Tienes que indicar tu usuario\n");
+    }
+    else
+    {
+
+        
+    }
+}
